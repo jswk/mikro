8ebccb4b1e650f2004feecba51dbde9c19538d0f
Jakub Sawicki <kuba@jswk.pl>
2013-11-24 00:02:27 +0100

    Packet length catcher

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,36 @@
  */
 
 #include <Arduino.h>
+#include "enc28j60.h"
+
+byte ENC28J60::buffer[1024];
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
 void setup()   {
-  pinMode(13, OUTPUT);
+	ENC28J60::initialize(1024, mymac);
+	ENC28J60::disableMulticast();
+	Serial.begin(9600);
 }
 
 
 void loop()
 {
+	uint16_t len;
 
-	digitalWrite(13, HIGH);
-	delay(100);
-	digitalWrite(13, LOW);
-	delay(1000);
+	do {
+		len = ENC28J60::packetReceive();
+	} while (len == 0);
+
+	Serial.println(len);
 
 }
 
+int main(void) {
 
+  init();
+  setup();
+
+  while(true) {
+    loop();
+  }
+}
